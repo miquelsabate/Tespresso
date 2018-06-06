5c57b466a6a3701dd88b4c815c52956cc2c3444b
Miquel Sabaté <miquel.sabate@estudiants.urv.cat>
2018-06-07 00:31:02 +0200

    Estil avançat (categories + productes + navbar completa)

--- Tespresso/db/seeds.rb
+++ Tespresso/db/seeds.rb
@@ -6,9 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-c1 = Category.create(name: "Capsulas", image: "https://i.pinimg.com/originals/4a/71/fe/4a71fe3d2bc350ef22f576236e59d575.png", description: "frtghy")
-c2 = Category.create(name: "Maquinas", image: "https://www.haceb.com/DesktopModules/Revindex.Dnn.RevindexStorefront/Portals/0/Gallery/cd39434a-9055-4b11-8851-54e907dea74e.jpg" , description: "frtghy")
-c3 = Category.create(name: "Accesorios", image: "https://www.nespresso.com/ecom/medias/sys_master/public/8887718903838/UK-5206-mediaMain.png", description: "frtghy")
+# c1 = Category.create(name: "Capsulas", image: "https://i.pinimg.com/originals/4a/71/fe/4a71fe3d2bc350ef22f576236e59d575.png", description: "frtghy")
+# c2 = Category.create(name: "Maquinas", image: "https://www.haceb.com/DesktopModules/Revindex.Dnn.RevindexStorefront/Portals/0/Gallery/cd39434a-9055-4b11-8851-54e907dea74e.jpg" , description: "frtghy")
+# c3 = Category.create(name: "Accesorios", image: "https://www.nespresso.com/ecom/medias/sys_master/public/8887718903838/UK-5206-mediaMain.png", description: "frtghy")
+
+c1 = Category.create(name: "Cápsulas", image: "/assets/capsulas-tespresso.png", description: "Cápsulas")
+c2 = Category.create(name: "Máquinas", image: "/assets/maquinas-tespresso.png" , description: "Máquinas")
+c3 = Category.create(name: "Accesorios", image: "/assets/accesorios-tespresso.png", description: "Accesorios")
 
 
 p1 = Product.create(name: "capsulaverde1", price: 20.01, description: "desc1", image: "https://i.pinimg.com/originals/4a/71/fe/4a71fe3d2bc350ef22f576236e59d575.png", category_id: c1.id)
@@ -21,4 +25,3 @@ p6 = Product.create(name: "Accesorio1", price: 20.01, description: "rgththghgh",
 u1 = User.create(email: "user1@gmail.com", password: "holahola")
 Order.create(user_id: u1.id, product_id: p1.id )
 Order.create(user_id: u1.id, product_id: p2.id )
-
